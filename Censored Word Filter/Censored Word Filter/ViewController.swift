542f27e8e15988212f6f7c182f06576b95a4aaa9
Andy Wong <wongandy97@gmail.com>
2016-07-08 23:32:36 -0400

    Add API call for Bad Word Filter; Add Text View, Text Field, and a function where when user taps return, it will complete the call and bring the keyboard down, Text View will show what the result and text view will allow user input

--- Censored Word Filter/Censored Word Filter/ViewController.swift
+++ Censored Word Filter/Censored Word Filter/ViewController.swift
@@ -7,17 +7,50 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
+import AlamofireNetworkActivityIndicator
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UITextFieldDelegate{
+
+    @IBOutlet weak var userInputTextField: UITextField!
+    
+    @IBOutlet weak var userOutputTextView: UITextView!
+    
+    
+    
+    var parameter = ["content": "Hello Fuck"]
+    let header = ["X-Mashape-Key": "FjNvux1QkHmshi1BJXIR7Zz3clnKp1SebTfjsnSgsju7Ebd3Jn"]
+    
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool // called when 'return' key pressed. return NO to ignore.
+    {
+        parameter = ["content": userInputTextField.text!]
+        
+        let apiToConnect = "https://neutrinoapi-bad-word-filter.p.mashape.com/bad-word-filter"
+        
+        Alamofire.request(.GET, apiToConnect, parameters: parameter, headers: header ).responseJSON{ response in
+            let responseJSON = response.result.value
+            print (responseJSON)
+            self.userOutputTextView.text = String(responseJSON!["bad-word-filter"])
+        }
+        
+        textField.resignFirstResponder()
+        return true;
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.userInputTextField.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
 
