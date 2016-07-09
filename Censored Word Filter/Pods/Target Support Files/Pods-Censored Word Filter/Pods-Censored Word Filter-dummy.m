99c6562d48734765528bb8d1c6114cef3d3e719e
Andy Wong <wongandy97@gmail.com>
2016-07-08 22:59:24 -0400

    Add pod files to project along with install pods for SwiftyJSON and Alamofire

---
+++ Censored Word Filter/Pods/Target Support Files/Pods-Censored Word Filter/Pods-Censored Word Filter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Censored_Word_Filter : NSObject
+@end
+@implementation PodsDummy_Pods_Censored_Word_Filter
+@end
---
+++ Censored Word Filter/Pods/Target Support Files/Pods-Censored Word Filter/Pods-Censored Word Filter-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Censored_Word_FilterVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Censored_Word_FilterVersionString[];
+
